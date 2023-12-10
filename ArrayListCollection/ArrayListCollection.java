c23a3c751a9e985c414d867024d48fd2826403e2
AbrarZahid050 <abrar.zhid050@gmail.com>
2023-12-10 16:35:29 -0500

    ArrayList done!

---
+++ ArrayListCollection/ArrayListCollection.java
@@ -0,0 +1,13 @@
+package ArrayListCollection;
+
+public class ArrayListCollection {
+    private int number = 3;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
