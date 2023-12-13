4acc91eda2e851747b13e5cc5afd39b5bb5b1774
AbrarZahid050 <abrar.zhid050@gmail.com>
2023-12-12 19:19:58 -0500

    inheritance protected and good engineering practice done

---
+++ BasePlusCommissionEmployee.java
@@ -0,0 +1,38 @@
+
+public class BasePlusCommissionEmployee extends CommissionEmployee {
+  private double baseSalary;
+
+  public BasePlusCommissionEmployee(String first, String last, String ssn, double sales, double rate, double salary) {
+
+    // explicit call to superclass CommissionEmployee constructor
+    super(first, last, ssn, sales, rate);
+
+  }
+
+  public void setBaseSalary(double salary) {
+    if (salary >= 0.0) {
+      baseSalary = salary;
+    } else {
+      throw new IllegalArgumentException("Base salary must be >= 0.0");
+    }
+  }
+
+  public double getBaseSalary() {
+    return baseSalary;
+  }
+
+  @Override
+  public double earnings() {
+    return baseSalary + (getCommissionRate() * getGrossSales());
+  }
+
+  @Override
+  public String toString() {
+    return String.format(
+        "%s: %s %s\n%s: %s\n%s: %.2f\n%s: %.2f\n%s: %.2f",
+        "base-salaried commission employee", getFirstName(), getLastName(),
+        "social security number", getSocialSecurityNumber(),
+        "gross sales", getGrossSales(), "commission rate", getCommissionRate(),
+        "base salary", getBaseSalary());
+  }
+}
---
+++ CommissionEmployeeTest.java
@@ -0,0 +1,24 @@
+public class CommissionEmployeeTest {
+    public static void main(String[] args) {
+        CommissionEmployee employee = new CommissionEmployee(
+                "John",
+                "Smith",
+                "123-456-7890",
+                10000,
+                .06);
+
+        System.out.println("Employee information obtained by get methods; \n");
+
+        System.out.printf("%s %s\n", "First name is", employee.getFirstName());
+        System.out.printf("%s %s\n", "Last name is", employee.getLastName());
+        System.out.printf("%s %s\n", "Social Security Number is", employee.getSocialSecurityNumber());
+        System.out.printf("%s %s\n", "Gross sales is", employee.getGrossSales());
+        System.out.printf("%s %s\n", "Commission rate is", employee.getCommissionRate());
+
+        employee.setGrossSales(500);
+        employee.setCommissionRate(.1);
+
+        System.out.printf("\n%s: \n\n%s\n", "Updated employee information obtained by toString", employee);
+
+    }
+}
