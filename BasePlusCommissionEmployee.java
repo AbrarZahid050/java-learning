ba50d925e6c1d7b7dadd17188a69bf204a54f79a
AbrarZahid050 <abrar.zhid050@gmail.com>
2023-12-14 10:52:08 -0500

    chapter 10 in progress - polymorphism

--- BasePlusCommissionEmployee.java
+++ BasePlusCommissionEmployee.java
@@ -6,7 +6,7 @@ public class BasePlusCommissionEmployee extends CommissionEmployee {
 
     // explicit call to superclass CommissionEmployee constructor
     super(first, last, ssn, sales, rate);
-
+    setBaseSalary(salary);
   }
 
   public void setBaseSalary(double salary) {
@@ -23,16 +23,13 @@ public class BasePlusCommissionEmployee extends CommissionEmployee {
 
   @Override
   public double earnings() {
-    return baseSalary + (getCommissionRate() * getGrossSales());
+    // return baseSalary + (getCommissionRate() * getGrossSales());
+    return getBaseSalary() + super.earnings();
   }
 
   @Override
   public String toString() {
-    return String.format(
-        "%s: %s %s\n%s: %s\n%s: %.2f\n%s: %.2f\n%s: %.2f",
-        "base-salaried commission employee", getFirstName(), getLastName(),
-        "social security number", getSocialSecurityNumber(),
-        "gross sales", getGrossSales(), "commission rate", getCommissionRate(),
-        "base salary", getBaseSalary());
+    return String.format("%s %s\n%s: %.2f", "base-salaried",
+        super.toString(), "base salary", getBaseSalary());
   }
 }
