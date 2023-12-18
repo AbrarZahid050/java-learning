69bed333742ae9e8125f9cc780e1a54c4e738ebe
AbrarZahid050 <abrar.zhid050@gmail.com>
2023-12-18 08:02:08 -0500

    upcasting and downcasting example done :q done
    "
    
    .
    "

--- Upcasting.java
+++ Upcasting.java
@@ -2,6 +2,10 @@ class Animal {
   public void eat() {
     System.out.println("-> [Animal-class-eat()] Animal eats");
   }
+
+  public void growl() {
+    System.out.println("-> [Animal-class-growl()] Animal growls");
+  }
 }
 
 class Dog extends Animal {
@@ -18,18 +22,24 @@ class Dog extends Animal {
 class Upcasting {
   public static void main(String[] args) {
     Animal animal = new Animal();
-    // Dog dog = new Dog();
+    Dog dog = new Dog();
     // animal = dog; // superClass variable -> subclass variable (upcasting)
     // animal.eat();
 
-    Animal myAnimal = new Dog(); // upcasting
+    Animal myAnimal = new Dog(); // Explicit upcasting
     doAnimalStuff(myAnimal);
     doAnimalStuff(animal);
-
+    doAnimalStuff(dog); // Implicit upcasting.
   }
 
   public static void doAnimalStuff(Animal animal) {
     System.out.println("-> [main-doAnimalStuff()]");
     animal.eat();
+
+    if (animal instanceof Dog) { // checking if the animal variable is an instance of Dog
+      Dog myDog = (Dog) animal; // Downcasting:
+      myDog.bark();
+    }
+    System.out.println();
   }
 }
