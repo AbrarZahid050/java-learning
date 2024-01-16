605bc98cd0715078f90a1be28c58aab5bfa6ae2b
AbrarZahid050 <abrar.zahid050@gmail.com>
2024-01-16 16:19:16 -0500

    after refactoring.

--- src/main/java/com/networking/Server.java
+++ src/main/java/com/networking/Server.java
@@ -11,31 +11,37 @@ public class Server {
   public static void main(String[] args) {
     final int port = 5555;
 
-    try {
-      ServerSocket server = new ServerSocket(port);
+    try (ServerSocket server = new ServerSocket(port)) {
       System.out.println("Server is listening at port: " + port);
 
       while (true) {
-        Socket client = server.accept();
-        PrintWriter out = new PrintWriter(client.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-
-        System.out.println("Client connected.");
-
-        String input;
-        while ((input = in.readLine()) != null) {
-          System.out.println("Received from client: " + input);
-          out.println("Server: " + input);
-
-          if (input == "bye") {
-            client.close();
-          }
-        }
+        clientConnect(server);
       }
     } catch (IOException err) {
       System.out.println("Could not start server. " + err.getMessage());
 
     }
+  }
 
+  public static void clientConnect(ServerSocket server) {
+    try (Socket client = server.accept();
+        PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));) {
+
+      System.out.println("Client connected.");
+
+      String input;
+      while ((input = in.readLine()) != null) {
+        System.out.println("Received from client: " + input);
+        out.println("Server: " + input);
+
+        if (input == "bye") {
+          client.close();
+        }
+      }
+    } catch (IOException err) {
+      System.err.println("Error in communication with the client");
+      err.printStackTrace();
+    }
   }
 }
