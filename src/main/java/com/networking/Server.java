81223d589a00737640c35a5abf8ac3408f3c1350
AbrarZahid050 <abrar.zahid050@gmail.com>
2024-01-18 17:31:14 -0500

    1-Created FatJar. 2-networking code. 3-Jackson-databind integrated.

--- src/main/java/com/networking/Server.java
+++ src/main/java/com/networking/Server.java
@@ -15,33 +15,40 @@ public class Server {
       System.out.println("Server is listening at port: " + port);
 
       while (true) {
+        // Continuously accept incoming client connections
         clientConnect(server);
       }
     } catch (IOException err) {
       System.out.println("Could not start server. " + err.getMessage());
-
     }
   }
 
+  // Method to handle communication with a connected client
   public static void clientConnect(ServerSocket server) {
     try (Socket client = server.accept();
         PrintWriter out = new PrintWriter(client.getOutputStream(), true);
         BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));) {
 
       System.out.println("Client connected.");
+      // Send initial message to the client
+      out.println("[Server] type 'bye' to exit.");
 
       String input;
       while ((input = in.readLine()) != null) {
+        // Process messages received from the client
         System.out.println("Received from client: " + input);
-        out.println("Server: " + input);
+        out.println("[Server] " + input);
 
-        if (input == "bye") {
+        // Check if the client wants to exit
+        if ("bye".equalsIgnoreCase(input.trim())) {
+          System.out.println("[Closing]--------------------");
           client.close();
+          // Exit the loop if 'bye' is received
+          break;
         }
       }
     } catch (IOException err) {
-      System.err.println("Error in communication with the client");
-      err.printStackTrace();
+      System.err.println("Ending the communication with the client.");
     }
   }
 }
